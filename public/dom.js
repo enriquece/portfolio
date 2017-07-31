d320b7e174c57317a0ae85822bec69501c9f150e
Your Name <email@example.com>
2017-07-31 16:33:21 -0400

    comment

---
+++ public/dom.js
@@ -0,0 +1,18 @@
+// window.addEventListener('load',function() {
+// // console.log(document.getElementById('our-text-input'));
+//   var textField = document.getElementById('our-text-input');
+//   console.log(textField);
+//   textField.addEventListener('change',function(event) {
+//     console.log("Something changed.");
+//     console.log(event.target.value);
+//   });
+//   });
+
+  // function onChangeHandler(input) {
+  //   console.log("Something changed");
+  //   console.dir(input.value);
+  // }
+window.addEventListener('load', function() {
+var textField = document.getElementById('our-text-input');
+textField.value = "A default value";
+})
---
+++ public/hellojs.js
@@ -0,0 +1,145 @@
+// console.log("I'm in a JS file!");
+//
+// //Single-line comment
+//
+// var some_var;
+//
+// /* multi
+// line comment
+// */
+// var some_other_var;
+//
+// var input;
+// if (input) {
+//   console.log(input);
+// } else {
+//   console.log("It's not defined.");
+// }
+//
+// var test = false;
+// var output = test ? "Passed the test" : "Failed the test";
+// console.log(output);
+//
+// var value = 0;
+// if (value >= 100) {
+//   console.log("The future rocks");
+// } else if (value > 100) && value > 1 {
+//   console.log("The future is pretty cool");
+// } else if (value === 1){
+//   console.log("meh");
+// } else {
+//   console.log("The future is boring");
+// }
+
+// var value = 0;
+// switch (value) {
+//   case 0:
+//   break;
+//   case 1:
+//   break;
+//   default:
+//   break;
+// }
+
+// var a = 0;
+// while (true) {
+//   console.log(a);
+// if (a >= 10) {
+//   break;
+// } else {
+//   a++;
+// }
+// }
+//
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+//
+// var arr = ["a","b","c","d"];
+//
+// arr.forEach(function(item) {
+//   console.log(item);
+//
+// });
+//
+// function my_func_stmt(arg) {
+//   console.log("inside my function");
+//   console.log(arg);
+// }
+//
+// var my_func_exp = function(arg) {
+//   console.log("inside my function expression");
+//   console.log(arg);
+// }
+//
+// my_func_stmt("Hola");
+// my_func_exp("Hola");
+//
+// var one = function() {
+//   return 1;
+// }
+// console.log(one());
+//
+// function addArgs() {
+//   var sum = 0;
+//   for(var i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
+//   return sum;
+// }
+// console.log(addArgs(3, 0.13, 0.01));
+//
+// var classroom = ({name: "alex", age: 22},{name: "Enrique", age: 24},{name: "Mark", age: 39})
+//
+// 
+// }
+//
+// function times(number) {
+//   for (i = 0; i <= number; i++);{
+//   console.log(i + "mph!");
+//   if (number === 88) {
+//     console.log("ZOOOOOM");
+//     }
+//   }
+// }
+//
+// console.log(a_var === undefined);
+// var a_var = 123;
+// // console.log(func_var === undefined);
+//
+// function a_function() {
+//   console.log(func_var === undefined);
+//   var func_var = "func";
+// }
+//
+// a_function();
+
+
+
+// function scope_func() {
+//   var my_own_var = "hellojs1";
+//   window.hello1 = function() {
+//     console.log(my_own_var);
+//   };
+// }
+// scope_func();
+//
+//
+// function times(how_many, block){
+//     for(i=0; i<=how_many; i++){
+//         block(i);
+//     }
+// }
+// undefined
+// function special_logger() {
+//     var day = new Date();
+//     return function(arg){
+//         console.log("It's "+day+" "+arg);
+//     }
+// }
+//
+// times(5, special_logger())
+
+function daysAgo(date){
+
+}
